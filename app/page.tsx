410bca43f4992705e4be0bfe4fb6881609432e84
Matthew Rhoads <78577112+mematthew123@users.noreply.github.com>
2024-04-23 09:02:01 -0600

    created new get started component, updaed filter ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
-import Banner from '@/components/Banner';
-import BenefitsComponent from '@/components/Benefits';
-import ClipSideBySideComponent from '@/components/ClipSidebySide';
-import CallToActionComponent from '@/components/Cta';
-import FaqsComponents from '@/components/FaqsComponent';
-import HeroComponent from '@/components/Hero';
-import ReviewsComponent from '@/components/Reviews';
-import SideContent from '@/components/SideContent';
-import StepsComponent from '@/components/Steps';
-import Steps from '@/components/Steps';
-import { client } from '@/sanity/lib/client';
-import { faqsQuery } from '@/sanity/lib/queries';
+import Banner from "@/components/Banner";
+import BenefitsComponent from "@/components/Benefits";
+import ClipSideBySideComponent from "@/components/ClipSidebySide";
+import CallToActionComponent from "@/components/Cta";
+import FaqsComponents from "@/components/FaqsComponent";
+import GetStarted from "@/components/GetStarted";
+import GirdStats from "@/components/GirdStats";
+import HeroComponent from "@/components/Hero";
+import ReviewsComponent from "@/components/Reviews";
+import SideContent from "@/components/SideContent";
+import StepsComponent from "@/components/Steps";
+import Steps from "@/components/Steps";
+import { client } from "@/sanity/lib/client";
+import { faqsQuery } from "@/sanity/lib/queries";
 
 export const revalidate = 3; // 3 seconds
 
@@ -19,6 +21,7 @@ export default async function Page() {
   return (
     <>
       <HeroComponent />
+      <GetStarted />
       {/* <SideContent /> */}
       {/* <StepsComponent />
       <BenefitsComponent /> */}
--- components/CategorySelector.tsx
+++ components/CategorySelector.tsx
@@ -20,16 +20,26 @@ const CategorySelector = ({ onSelectCategory }: { onSelectCategory: (category: s
   }, []);
 
   return (
-    <div>
-      <select onChange={(e) => onSelectCategory(e.target.value)} defaultValue="">
-        <option className='w-64 p-4 rounded-lg' value="">Select a category</option>
+    <div className='relative'>
+    <select 
+        className='w-full bg-secondary-400 text-secondary-950 p-4 rounded-lg appearance-none focus:outline-none focus:ring-2 focus:ring-secondary-600 focus:border-transparent'
+        onChange={(e) => onSelectCategory(e.target.value)} 
+        defaultValue=""
+    >
+        <option value="" disabled>Select a category</option>
         {categories.map((category) => (
           <option key={category._id} value={category._id}>
             {category.title}
           </option>
         ))}
-      </select>
+    </select>
+    <div className='pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700'>
+        <svg className='fill-current h-4 w-4' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
+            <path d='M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z' />
+        </svg>
     </div>
+</div>
+
   );
 };
 
